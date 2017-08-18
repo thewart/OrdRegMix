6a809afd46b9ceb247b7e6947d0779166bf35b51
thewart <seth.madlonkay@gmail.com>
2017-08-18 01:02:56 -0400

    multidimensional regression in julia

--- ordreg.jl
+++ ordreg.jl
@@ -1,41 +1,38 @@
 import DataArrays.cut
 using FreqTables.freqtable
 using DataFrames, Distributions
-n = 1000;
+n = 500;
 p = 4;
-v
-α0 = -0.25;
+α0 = -0.5;
 β0 = randn(p);
-X = randn(p,n);
+Xf = randn(p,n);
 cp = [-Inf, 0, 2.0, Inf];
-Z = [ModelMatrix(ModelFrame(@formula(y~0+ X),DataFrame(y=fill(0,n),
+Xr = [ModelMatrix(ModelFrame(@formula(y~0+ X),DataFrame(y=fill(0,n),
   X=@pdata(repeat(vcat(1:50),inner=div(n,50)))))).m'];
-σ2_u = 1.5;
+σ2_u = 1;
 u0 = sqrt(σ2_u) * randn(50);
-v = length(Z);
+v = length(Xr);
 
-y = cut(α0 + X'β0 + Z[1]'u0 + randn(n),cp);
+y = cut(α0 + Xf'β0 + Xr[1]'u0 + randn(n),cp);
 setlevels!(y,map(string,1:(length(cp)-1)));
 y = map(parse,y);
 
 iter = 1000;
-τ_β = 5.0;
-ν0 = 0.01;
-δ0 = 0.01;
-σ0_u = 0.01;
-ν0_u = 0.01;
+prior[:σ2_β] = 5.0;
+prior[:σ2_α] = 5.0;
+prior[:τ0_δ] = 0.01;
+prior[:ν0_δ] = 0.01;
+prior[:τ0_u] = 0.01;
+prior[:ν0_u] = 0.01;
 γ = [-Inf,0,1,Inf];
 l = size.(Z,1);
 
-Lβ = inv( chol(X*X' + I*inv(τ_β)) );
-ΣβX = Lβ*Lβ'*X;
-
 samp = Dict{Symbol,AbstractArray}();
-samp[:β] = Array{Float64,2}(p,iter);
-samp[:α] = Vector{Float64}(iter);
-samp[:δ2] = Vector{Float64}(iter);
-samp[:u] = Array{Vector{Float64}}(v,iter);
-samp[:σ2_u] = Array{Float64,2}(v,iter);
+samp[:β] = Array{Float64,3}(p,dim,iter);
+samp[:α] = Array{Float64,2}(dim,iter);
+samp[:δ2] = Array{Float64,2}(dim,iter);
+samp[:u] = Array{Vector{Float64},3}(v,dim,iter);
+samp[:σ2_u] = Array{Float64,3}(v,dim,iter);
 
 α = randn();
 β = randn(p);
@@ -48,36 +45,16 @@ for i in 1:n
 end
 
 for t in 1:iter
-  α = sum(z-X'β-calcZtu(Z,u))/(n+ν0) + sqrt(δ2)/(n+ν0) * randn();
-
-  β = ΣβX*(z.-α-calcZtu(Z,u)) + sqrt(δ2)*Lβ*randn(p);
-
-  for i in 1:v
-    otherz = setdiff(1:v,i);
-    Lu = inv( chol(Z[i]*Z[i]' + I*inv(σ2_u[i])) );
-    u[i] = Lu*Lu'*Z[i]*(z.-α-X'β-calcZtu(Z[otherz],u[otherz])) +
-     sqrt(δ2)*Lu*randn(l[i]);
-
-    a = 0.5(l[i]+ν0_u);
-    b = 0.5(σ0_u*ν0_u + dot(u[i],u[i])/δ2);
-    σ2_u[i] = rand(InverseGamma(a,b));
+  for d in 1:dim
+    z[:,d], α[d], β[:,d], u[:,d], σ2_u[:,d], δ2[d] =
+          sample_rate(y[d],z[:,d],Xf,Xr,α[d],β[:,d],u[:,d],σ2_u[:,d],γ,δ2[d],prior)
   end
 
-  η = α + X'β + calcZtu(Z,u);
-  for i in 1:n
-    z[i] = rand( Truncated( Normal(η[i],sqrt(δ2)), γ[y[i]],γ[y[i]+1]) );
-  end
-
-  ss = z - α - X'β - calcZtu(Z,u);
-  a = 0.5*(n+ν0);
-  b = 0.5*(δ0*ν0 + dot(ss,ss));
-  δ2 = rand(InverseGamma(a,b));
-
-  samp[:β][:,t] = β;
-  samp[:α][t] = α;
-  samp[:δ2][t] = δ2;
-  samp[:u][:,t] = u;
-  samp[:σ2_u][:,t] = σ2_u;
+  samp[:β][:,:,t] = β;
+  samp[:α][:,t] = α;
+  samp[:δ2][:,t] = δ2;
+  samp[:u][:,:,t] = deepcopy(u);
+  samp[:σ2_u][:,:,t] = σ2_u;
 end
 
 
@@ -89,4 +66,36 @@ function calcZtu(Z,u)
     Ztu=0;
   end
   return Ztu
+end
+
+function sample_rate(y,z,Xf,Xr,α,β,u,σ2_u,γ,δ2,prior)
+
+  α = sum(z-Xf'β-calcZtu(Xr,u))/(n+inv(prior[:σ2_α])) +
+      sqrt(δ2/(n+inv(prior[:σ2_α]))) * randn();
+
+  Lβ = inv( chol(Xf*Xf' + I*inv(prior[:σ2_β])) );
+  β = Lβ*lβ'*Xf*(z.-α-calcZtu(Xr,u)) + sqrt(δ2)*Lβ*randn(p);
+
+  for i in 1:v
+    otherz = setdiff(1:v,i);
+    Lu = inv( chol(Xr[i]*Xr[i]' + I*inv(σ2_u[i])) );
+    u[i] = Lu*Lu'*Xr[i]*(z.-α-Xf'β-calcZtu(Z[otherz],u[otherz])) +
+     sqrt(δ2)*Lu*randn(l[i]);
+
+    a = 0.5(l[i]+prior[:ν0_u]);
+    b = 0.5(prior[:σ0_u]*prior[:ν0_u] + dot(u[i],u[i])/δ2);
+    σ2_u[i] = rand(InverseGamma(a,b));
+  end
+
+  η = α + Xf'β + calcZtu(Xr,u);
+  for i in 1:n
+    z[i] = rand( Truncated( Normal(η[i],sqrt(δ2)), γ[y[i]],γ[y[i]+1]) );
+  end
+
+  ss = z - α - Xf'β - calcZtu(Xr,u);
+  a = 0.5*(n+prior[:ν0_δ]);
+  b = 0.5*(prior[:δ0_δ]*prior[:ν0_δ] + dot(ss,ss));
+  δ2 = rand(InverseGamma(a,b));
+
+  return z, α, β, u, σ2_u, δ2
 end
